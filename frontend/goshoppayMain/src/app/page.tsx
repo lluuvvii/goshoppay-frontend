8f0bb43192970558b3b46fb23d18ca40a60b0d2e
lluuvvii <kurniawanluvi64@gmail.com>
2024-01-14 18:45:55 +0700

    make base layout home page

--- frontend/goshoppayMain/src/app/page.tsx
+++ frontend/goshoppayMain/src/app/page.tsx
@@ -1,6 +1,13 @@
+import Product from "@/components/Product"
+
 const Home = async () => {
   return (
-    <div className='bg-color-primary'>HOME</div>
+    <div className='bg-gray-300'>
+      <div className='flex justify-center items-center'>
+        <h1>HOME</h1>
+      </div>
+      <Product />
+    </div>
   )
 }
 
---
+++ frontend/goshoppayMain/src/components/Product/index.tsx
@@ -0,0 +1,16 @@
+const Product = () => {
+  return (
+    <div className='grid grid-cols-4 gap-2 h-40'>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+      <div className='bg-color-primary'>Product</div>
+    </div>
+  )
+}
+
+export default Product
